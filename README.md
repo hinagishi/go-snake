36248375e5f193dd58fa097f51a7c616c03e6229
hinagishi <archangel.ratziel@gmail.com>
2016-01-01 18:20:33 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## go-snake
+WIP
+
+## Requirement
+github.com/nsf/termbox-go
+
+
+
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"github.com/nsf/termbox-go"
+	"time"
+)
+
+type Point struct {
+	X int
+	Y int
+}
+
+type Snake struct {
+	Elm []Point
+	Dir int // 0: up 1: down 2: right 3: left
+}
+
+func draw(s *Snake) {
+	for {
+		termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+		for _, e := range s.Elm {
+			termbox.SetCell(e.X, e.Y, '@', termbox.ColorDefault, termbox.ColorDefault)
+		}
+		termbox.Flush()
+		s.updatePos()
+		time.Sleep(300 * time.Millisecond)
+	}
+}
+
+func (s *Snake) updatePos() {
+	x := s.Elm[0].X
+	y := s.Elm[0].Y
+	switch s.Dir {
+	case 0:
+		y--
+	case 1:
+		y++
+	case 2:
+		x++
+	case 3:
+		x--
+	}
+	s.Elm = append([]Point{Point{x, y}}, s.Elm[0:len(s.Elm)-1]...)
+	return
+}
+
+func initSnake() Snake {
+	var s Snake
+	s.Elm = []Point{Point{20, 20}, Point{20, 19}, Point{20, 18}}
+	s.Dir = 1
+	return s
+}
+
+func main() {
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+	snake := initSnake()
+	go draw(&snake)
+	for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			switch ev.Key {
+			case termbox.KeyEsc:
+				return
+			case termbox.KeyArrowUp:
+				snake.Dir = 0
+			case termbox.KeyArrowDown:
+				snake.Dir = 1
+			case termbox.KeyArrowRight:
+				snake.Dir = 2
+			case termbox.KeyArrowLeft:
+				snake.Dir = 3
+			}
+		}
+	}
+}
